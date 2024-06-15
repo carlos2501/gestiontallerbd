1f31b2d41752b8183cce829a167e71908b7e5d44
CPL <carlos@centropyme.com>
2024-06-15 13:40:44 +0200

    * Creada relación M2O (muchos a uno entre Albarán y Cliente: varios albaranes tienen el mismo cliente. CASO DE USO: Esto indica un cliente que ha llevado el coche al taller en varias ocasiones y en cada una de ellas se le ha generado un nuevo albarán * Creado el "main" para probar la relación M2O (many to one)

--- src/main/java/org/cplcursos/java/Entidades/Albaran.java
+++ src/main/java/org/cplcursos/java/Entidades/Albaran.java
@@ -21,6 +21,20 @@ public class Albaran {
    private LocalDateTime fecha;
    private Float importe;
 
+   /*
+        @ManyToOne hace que JPA defina una relación de muchos a uno entre Albaran -la parte de "muchos" puesto que es
+            la que incluye la anotación @ManyToOne- y Cliente -la parte de "uno".
+
+            Esta relación indica que varios albaranes pueden tener el mismo cliente.
+
+            El CASO DE USO sería que un cliente visita varias veces el taller y se le generan varios albaranes distintos.
+
+        @JoinColumn indica qué columna de la parte de "muchos" se utilizará para la clave foránea. Si no se indica nada,
+            JPA creará una columna del mismo nombre de la propiedad añadiendo un "_id" al final.
+
+            Así mismo JPA crea la clave foránea (en la tabla "albaranes") apuntando a la tabla "clientes" con los campos
+            "id_cliente" e "id" -clave primaria de "clientes"- respectivamente
+    */
    @ManyToOne
    @JoinColumn(name = "id_cliente")
    private Cliente cliente;
--- src/main/java/org/cplcursos/java/HibernateRelacionM2O.java
+++ src/main/java/org/cplcursos/java/HibernateRelacionM2O.java
@@ -2,13 +2,51 @@ package org.cplcursos.java;
 
 import jakarta.persistence.EntityManager;
 import org.cplcursos.java.Entidades.Albaran;
+import org.cplcursos.java.Entidades.Cliente;
+import org.cplcursos.java.Servicios.ServicioApp;
+import org.cplcursos.java.Servicios.ServicioAppImpl;
 import org.cplcursos.java.util.JpaUtil;
+
+import java.time.LocalDateTime;
 
 public class HibernateRelacionM2O {
     public static void main(String[] args) {
         // Creamos el Entitymanager en la capa que use el repositorio
         EntityManager em = JpaUtil.getEntitymanager();
+        // Creamos una instancia de la capa de servicio que vamos a utilizar
+        ServicioApp srvcapp = new ServicioAppImpl(em);
 
-        Albaran alb = new Albaran();
+        System.out.println("------------------ Añadir albarán a un cliente --------------");
+        try {
+            // Creamos un albarán que asignamos al cliente 6
+            Albaran alb = new Albaran();
+            alb.setFecha(LocalDateTime.now());
+            alb.setImporte(456.45F);
+            // cargamos los datos del cliente para asignaros al alabarán
+            Cliente cli = srvcapp.porId(6).get();
+            alb.setCliente(cli);
+
+            // Iniciamos la transacción
+            em.getTransaction().begin();
+            em.persist(alb);
+            /*
+                Como se ha definido una relación "muchos a uno" ente Albaran y Cliente, JPA asigna automáticamente
+                el id del cliente -propiedad "cliente.id"- al campo "id_cliente" de la tabla "albaranes" a la hora de
+                guardar los datos en la bbdd
+             */
+
+            // Volcamos las operaciones realizadas a la BBDD -transaction.commit()-
+            em.getTransaction().commit();
+            // Mostramos los datos del albarán
+            System.out.println(alb);
+        } catch (Exception e) {
+            // Deshacemos las operaciones pendientes anteriores
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            // Cerramos la EntityManager
+            em.close();
+        }
+
     }
 }
